03453f805f091bcc863d9ed14a9f06d9050c180b
monsite <lamraniamal161@gmail.com>
2023-12-11 12:39:11 +0100

    first commit

--- app.py
+++ app.py
@@ -6,4 +6,6 @@ def calculate_square(number):
 
 if __name__ == "__main__":
     print("Hello world !")
-    
+   n = input ("intrer le nombre") 
+   n2 = calculate_square(n)
+   priint(f"resultat is {n2}" )
