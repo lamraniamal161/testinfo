8a4edd839c46ae06261c14ca8ba3cdce54893a88
monsite <lamraniamal161@gmail.com>
2023-12-11 13:44:10 +0100

    add function

--- app.py
+++ app.py
@@ -3,9 +3,11 @@
 def calculate_square(number):
     return number * number
 
+def add(a,b):
+   return a+b ;
 
 if __name__ == "__main__":
     print("Hello world !")
-   n = input ("intrer le nombre") 
-   n2 = calculate_square(n)
-   priint(f"resultat is {n2}" )
+    n = float(   input("intrer le nombre")  )
+    n2 = calculate_square(n)
+    print(f"resultat is {n2}" )
